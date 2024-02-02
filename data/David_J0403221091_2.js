9df1001b2a59d704410fd471eca99a585e2c8dac
deviansky <125205877+deviansky@users.noreply.github.com>
2024-02-02 11:30:04 +0700

    Add files via upload

---
+++ data/David_J0403221091_2.js
@@ -0,0 +1,1 @@
+var json_David_J0403221091_2 = {"type":"FeatureCollection","name":"David_J0403221091_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","Kost":"Asrama Ekasari","harga":"150000"},"geometry":{"type":"Point","coordinates":[106.800870950042778,-6.589711613790264]}}]}
